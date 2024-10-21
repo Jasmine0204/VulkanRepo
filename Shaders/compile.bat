9ca04a134005c476d848632b57c8444e9931cd44
Jasmine Chen <70587225+Jasmine0204@users.noreply.github.com>
2024-10-21 10:04:09 +0800

    Delete Shaders directory

--- Shaders/compile.bat
+++
@@ -1,7 +0,0 @@
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe motion.vert -o motionvert.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe horizonshader.vert -o horizontalvert.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe verticalshader.vert -o verticalvert.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe motion.frag -o motionfrag.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe horizonshader.frag -o horizontalfrag.spv
-C:/VulkanSDK/1.3.275.0/Bin/glslc.exe verticalshader.frag -o verticalfrag.spv
-pause
